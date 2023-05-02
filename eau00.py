dd49439354ee8cfb7204a0062af33992593981eb
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-05-02 19:38:44 +0200

    fucking crap

--- eau00.py
+++ eau00.py
@@ -5,3 +5,40 @@
 #                                                                                                           répétition volontaire ( WTF ??? )
 
 "ce truc n'a ni queue ni tête !"
+
+import sys
+
+numList = []
+
+def removeDouble(arglist):
+    for x in arglist:
+        #if numList[0] == numList[1] or numList[1] == numList[2] or numList[0] == numList[2]:
+        if arglist[0] == arglist[1] == arglist[2]:
+            print(" double detected ! ")
+            arglist.remove(x)
+            print(arglist)
+
+"""def removeDouble(i, j, k):
+    for i, j, k in numList:
+        if numList[i] == numList[j] or numList[j] == numList[k] or numList[i] == numList[k]:
+            print(" double detected ! ")
+            numList.remove(i, j, k)"""
+
+def generateNum():
+    for i in range(0, 3):
+        for j in range(0, 3):
+            for k in range(0, 3):
+                numList = [i,j,k]
+                removeDouble(numList)
+                #print(numList)
+
+generateNum()
+removeDouble()
+#print(numList)
+
+# trier les "doublons"
+
+# transformer en nombre normaux pour présentation du résultat final
+# ', '.join(str(e) for e in numList)
+
+#print(numList)
--- eau01.py
+++ eau01.py
@@ -3,3 +3,27 @@
 # Créer un programme qui affiche toutes les différentes combinaisons de 2 nombres entre 00 et 99 dans l'ordre croissant.
 
 "ce truc n'a ni queue ni tête !"
+
+leftCount = 0
+rightCount = 0
+display = (" %d  %d " % (leftCount, rightCount))
+
+
+while leftCount <= 99 and rightCount <= 99:
+    
+    rightCount += 1
+    
+    if rightCount == 99 and leftCount < 99:
+        leftCount += 1
+        rightCount = 0
+        print(display)
+        continue
+    
+    if leftCount == 99 and rightCount == 99:
+        print("you have reached 99  99 and the end of the given possibility")
+        break
+    
+    else:
+        continue
+
+print(display)
