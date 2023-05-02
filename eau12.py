243d4bf8b6ccacf39c1a131fe014e7bbf4efcc14
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-05-02 16:22:06 +0200

    much more pleasant than the others crazy craps !

--- eau12.py
+++ eau12.py
@@ -4,9 +4,30 @@
 # implémentation du tri à bulle OBLIGATOIRE.
 # Afficher "ERROR" et quitter le programme en cas de problèmes d'arguments
 
-# voir sur Wikimerdia
+# voir sur Wikimerdia... ou pas !
 
-# my_bubble_sort (array) {
-#       write algo here
-#       return (new_array)
-# }
+import sys
+
+# exemple of use --->       "6 5 10 4 7 8 3 9 2 1"
+testInput = str(sys.argv[1])
+# this below turn the string of numbers into a list of integers INSTEAD OF a list of string numbers
+listToOrdo = list(map(int, testInput.split()))
+
+#listToOrdo = [6, 5, 10, 4, 7, 8, 3, 9, 2, 1]
+
+def bubbleSort(list):
+    permutation = True
+    passage = 0
+    while permutation:
+        permutation = False
+        passage = passage+1
+        for i in range(0, len(list)- passage):
+            print(list)
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+                permutation = True
+    
+    return list
+
+bubbleSort(listToOrdo)
+print(listToOrdo)
